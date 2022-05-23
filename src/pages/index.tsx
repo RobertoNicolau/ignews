d2da2ece37774dbcb5901cde324a15ed9338635a
Roberto Nicolau <roberto.leonardo.2000@gmail.com>
2022-05-23 09:37:09 -0300

    Aplicando conceitos de SSR e SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 import styles from "../styles/home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -17,9 +25,9 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $ 9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceID={product.priceId} />
         </section>
         <img src="/images/avatar.svg" alt="Gril Coding" />
       </main>
@@ -27,10 +35,22 @@ export default function Home() {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1L0tjWFDPEcCOjLYRfC0HWur", {
+    expand: ["product"],
+  });
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
   return {
     props: {
-      nome: "Roberto",
+      product,
     },
+    revalidate: 60 * 60 * 24, //24 hours
   };
 };
